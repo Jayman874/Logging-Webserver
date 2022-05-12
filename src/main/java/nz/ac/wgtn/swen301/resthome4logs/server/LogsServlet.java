627c24faafcb306f6368dab4c2ed5ee8c3270891
gulabjaye <gulabjaye@myvuw.ac.nz>
2022-05-12 18:07:02 +1200

    Added needed dependencies and plugins to pom.xml file. Created LogsServet class with method templates for doGet, doPost, doDelete, and created Persistency class.

---
+++ src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/LogsServlet.java
@@ -0,0 +1,34 @@
+package nz.ac.wgtn.swen301.resthome4logs.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogsServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public LogsServlet() {
+		
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	
+}
