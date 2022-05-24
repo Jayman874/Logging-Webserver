08b2b01aaae870b5833ccc5dc5306116dd3eb67f
gulabjaye <gulabjaye@myvuw.ac.nz>
2022-05-24 20:38:15 +1200

    Added static database to persistency class. This is a list. Added web.xml file which generates the servlet from the LogsServlet.java class with the url of logs.

--- src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/Persistency.java
@@ -1,5 +1,12 @@
 package nz.ac.wgtn.swen301.resthome4logs.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.spi.LoggingEvent;
+
 public class Persistency {
 
+	public static List<LoggingEvent> DB = new ArrayList<LoggingEvent>();
+	
 }
