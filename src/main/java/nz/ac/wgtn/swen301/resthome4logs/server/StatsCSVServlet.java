157e13294a56ccc411de98cdfbfd18b586e6f0c8
gulabjaye <gulabjaye@myvuw.ac.nz>
2022-05-31 03:59:32 +1200

    Created StatsCSVServlet, StatsServlet, and StatsXLSServlet classes templates which all extend HttpServlet. Added new servlets to web.xml which reference the newly created stats classes. These new urls are statscsv, stats, and statsxls.

---
+++ src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/StatsCSVServlet.java
@@ -0,0 +1,20 @@
+package nz.ac.wgtn.swen301.resthome4logs.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StatsCSVServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public StatsCSVServlet() {}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+}
